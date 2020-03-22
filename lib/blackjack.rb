b097fdaa861ca6d9dd9472fc4699c8de5b8b7527
John Tom Jr <johntjr.22@gmail.com>
2020-03-22 05:31:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,7 +68,7 @@ def runner
   until total > 21
   
   total = hit?(total)
-  
+  display_card_total(card_total)
   
 end
 end_game(total)
@@ -77,6 +77,5 @@ end_game(total)
 end
     
 
-    runner
 
 
